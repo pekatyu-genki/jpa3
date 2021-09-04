7fdb91950ca81849f4ff819d48d56eb7c64ce268
pekatyu-genki <pekatyu.genkidetyu@gmail.com>
2021-09-04 16:16:10 +0900

    社員情報クラス作成

---
+++ spring-jpa3/src/main/java/com/example/demo/model/Employee.java
@@ -0,0 +1,40 @@
+package com.example.demo.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * 社員情報クラス
+ * @author PCUser
+ *
+ */
+@Getter
+@Setter
+@Entity
+public class Employee {
+
+	/**
+	 * 社員ID
+	 */
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	/**
+	 * 名前
+	 */
+	@NotBlank
+	@Size(max = 40)
+	private String name;
+	
+	@ManyToOne
+	private Department department;
+}
