702eda64d2fd110d911f0069f04a703a5d80e5cc
pekatyu-genki <pekatyu.genkidetyu@gmail.com>
2021-09-05 23:36:02 +0900

    データローダークラス作成 pom.xmlに参照ライブラリを追記

---
+++ spring-jpa3/src/main/java/com/example/demo/config/DataLoader.java
@@ -0,0 +1,30 @@
+package com.example.demo.config;
+
+import org.springframework.stereotype.Component;
+
+import com.example.demo.model.Department;
+import com.example.demo.repository.DepartmentRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Component
+public class DataLoader {
+
+	/** 定数 部門情報用インターフェイス格納 */
+	private final DepartmentRepository repository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		// 部門クラス呼び出し(インスタンス化)
+		Department department = new Department();
+		
+		department.setName("営業");
+		repository.save(department);
+		
+		department = new Department();
+		department.setName("開発");
+		repository.save(department);
+	}
+}
